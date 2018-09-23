4dc2dc34f7a8d7f681f49ddd96a01210f79d8e64
bogdanbledea <bogdan.bledea@evozon.com>
2018-09-23 20:35:10 +0300

    created card

---
+++ app/src/components/card/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../card/Card.scss.";
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="card">
+                <h1>{this.props.number},{this.props.symbol}</h1>
+            </div>
+        )
+    };
+}
+
+export default Card;
